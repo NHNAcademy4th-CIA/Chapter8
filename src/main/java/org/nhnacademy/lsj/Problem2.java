473db8b14e2137e9b4a2d3b6913924f90e0a89d8
masiljangajji <hjk172262@gmail.com>
2023-09-20 18:30:08 +0900

    feat: Problem2 Solve

---
+++ src/main/java/org/nhnacademy/lsj/Problem2.java
@@ -0,0 +1,71 @@
+package org.nhnacademy.lsj;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 빅 인티저를 이용한 3N+1 문제 풀이.
+ */
+public class Problem2 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Problem2.class);
+
+    private static final BigInteger TWO = new BigInteger("2");
+    private static final BigInteger ONE = new BigInteger("1");
+    private static final BigInteger ZERO = new BigInteger("0");
+
+
+    /**
+     * 3N+1 문제풀이.
+     */
+    public static void problem2() {
+
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger bigInteger;
+
+        while (true) {
+
+            try {
+                bigInteger = new BigInteger(sc.nextLine());
+                if (!bigInteger.equals(bigInteger.abs())) {
+                    throw new IllegalArgumentException();
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+                logger.warn(e.getMessage());
+            }
+            logger.info("다시 입력해 주세요");
+
+        }
+
+        solve3N(bigInteger);
+
+    }
+
+
+    /**
+     * 짝수면 N/2 , 홀수면 N*3+1.
+     *
+     * @param bigInteger 1이되는지 확인할 숫자.
+     */
+    public static void solve3N(BigInteger bigInteger) {
+
+        logger.info("{}", bigInteger);
+
+        if (bigInteger.equals(ONE)) {
+            return;
+        }
+
+        if (bigInteger.remainder(TWO).equals(ZERO)) {
+            solve3N(bigInteger.divide(BigInteger.valueOf(2)));
+        } else if (bigInteger.remainder(TWO).equals(ONE)) {
+            solve3N(bigInteger.multiply(BigInteger.valueOf(3)).add(BigInteger.valueOf(1)));
+        }
+
+    }
+
+
+}
