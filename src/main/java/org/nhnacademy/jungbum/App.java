d7cfe03047ca6f8af2a98649f57a1bb6058d9ba3
LeeJungBum <jungbum05114@gmail.com>
2023-09-20 15:21:42 +0900

    feat: Quiz1

--- src/main/java/org/nhnacademy/jungbum/App.java
+++ src/main/java/org/nhnacademy/jungbum/App.java
@@ -1,4 +1,4 @@
-package org.nhnacademy;
+package org.nhnacademy.jungbum;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new Quiz1();
     }
 }
